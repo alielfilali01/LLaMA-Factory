41f1361f9818eca673cb8bcc8eb381a34b2a19f9
3 a l i <58257628+alielfilali01@users.noreply.github.com>
2024-05-25 17:36:13 +0100

    Update merge_and_push.py

--- examples/lora_multi_gpu/GMAL/merge_and_push.py
+++ examples/lora_multi_gpu/GMAL/merge_and_push.py
@@ -9,16 +9,18 @@ import argparse
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Merge and push adapter to Hugging Face Hub")
 parser.add_argument('--adapter_path', type=str, required=True, help="Path to the adapter")
-parser.add_argument('--base_model', type=str, required=True, help="Path to the base model")
+parser.add_argument('--base_model', type=str, required=True, help="Path to the base model on Hugging Face Hub")
+parser.add_argument('--token', type=str, required=True, help="Hugging Face token")
+parser.add_argument('--org', type=str, default="Ali-C137", help="Organization name or username on Hugging Face Hub")
 args = parser.parse_args()
 
 # Define paths and names
 ADAPTER_PATH = args.adapter_path
 BASE_MODEL = args.base_model
+TOKEN = args.token
+ORG = args.org
 ADAPTER_NAME = ADAPTER_PATH.replace('/', '-').replace('.', '') + "-adapter"
 MODEL_NAME = ADAPTER_PATH.replace('/', '-').replace('.', '') + "-model"
-ORG = "Ali-C137"
-TOKEN="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"
 
 # Load the tokenizer and base model
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
